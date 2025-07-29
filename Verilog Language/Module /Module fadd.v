ab7610e3f1380629417f1b9f66ef0345926d4724
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-07-29 14:45:49 +0530

    Update Module fadd.v

--- Verilog Language/Module /Module fadd.v
+++ Verilog Language/Module /Module fadd.v
@@ -22,3 +22,24 @@ If your submission is missing a module add1, you will get an error message that 
 
 ANSWER:
         
+                module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);//
+    wire[15:0] sum1,sum2;
+    wire  cout1;
+    wire cin1=1'b0;
+    add16 adder1(.a(a[15:0]),.b(b[15:0]),.cin(cin1),.sum(sum1),.cout(cout1));
+    add16 adder2(.a(a[31:16]),.b(b[31:16]),.cin(cout1),.sum(sum2),.cout());
+    assign sum={sum2,sum1};
+    
+endmodule
+
+module add1 ( input a, input b, input cin,output sum, output cout );
+    assign sum = a^b^cin;
+    assign cout = a&b|cin&b|a&cin;
+
+// Full adder module here
+
+endmodule
