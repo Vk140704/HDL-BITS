399988e3d4d9732f030f448f442605f325b502ac
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-05 15:19:06 +0530

    Create Gnd.v

---
+++ Circuits/Combinational/Gnd.v
@@ -0,0 +1,14 @@
+Implement the following circuit:
+            _ _ _ _ _ _ OUT
+            |
+            |
+    GND   -----
+           ---
+            -
+Answer:
+       module top_module (
+    output out);
+    wire w1;
+    assign w1 = 1'b0;
+assign out = w1;
+endmodule
