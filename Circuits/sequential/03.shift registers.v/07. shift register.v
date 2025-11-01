e8b4a8761c36be2aae1830f2c5658a3a931f770c
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-11-01 11:55:35 +0530

    Create 07. shift register.v

---
+++ Circuits/sequential/03.shift registers.v/07. shift register.v
@@ -0,0 +1,24 @@
+
+module top_module (
+    input clk,
+    input resetn,   // synchronous reset
+    input in,
+    output out);
+     reg q1,q2,q3,q4;
+    always@(posedge clk)begin
+        if(!resetn)begin
+           q1<=0;
+           q2<=0;
+           q3<=0;
+           q4<=0;
+          end
+        else begin
+            q1<=in;
+            q2<=q1;
+            q3<=q2;
+            q4<=q3;
+        end
+        
+    end
+    assign out=q4;
+endmodule
